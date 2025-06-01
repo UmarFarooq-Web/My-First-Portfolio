618ac4c0ad3a7a58a08043f04476fdb83bcf4b2b
Umar Farooq <umarfarooq.sudo@gmail.com>
2025-06-01 11:30:52 -0700

    f

--- script.js
+++ script.js
@@ -7,19 +7,46 @@ let AboutIcon = document.querySelector("#menuIcon2");
 let HomeContainer = document.getElementsByClassName("homeContainer")[0]; // Corrected the class name selection
 let aboutContainer = document.getElementsByClassName("aboutContainer")[0]
 
-AboutIcon.addEventListener("click", () => {
-    // alert("clicked");
-    HomeContainer.style.display = "none"; // Corrected the typo in the variable name
-    aboutContainer.style.display = "block";
 
-    AboutIcon.classList.add("activeIcon")
-    HomeIcon.classList.remove("activeIcon");
-});
+
+
+
 
 HomeIcon.addEventListener("click" , ()=>{
     aboutContainer.style.display = "none";
+
+
+    HomeContainer.classList.add("hidden");
     HomeContainer.style.display = "block";
+    void HomeContainer.offsetWidth;
+    HomeContainer.classList.add("show")
 
     AboutIcon.classList.remove("activeIcon")
     HomeIcon.classList.add("activeIcon");
+
+    aboutContainer.classList.remove("show");
+
 })
+
+AboutIcon.addEventListener("click", () => {
+    // alert("clicked");
+    HomeContainer.style.display = "none";
+    
+    
+    aboutContainer.classList.add("hidden")
+    aboutContainer.style.display = "block";
+    void aboutContainer.offsetWidth;
+    aboutContainer.classList.add("show");
+
+
+
+    AboutIcon.classList.add("activeIcon")
+    HomeIcon.classList.remove("activeIcon");
+
+    HomeContainer.classList.remove("show");
+
+
+    
+});
+
+
